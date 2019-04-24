18dbf4726d51a188346f08cda3ec5fbe80227da7
yeob <okayhan89@gmail.com>
2019-04-24 10:45:28 +0900

    1일차

---
+++ src/main/java/com/skcc/Test0411EduApplication.java
@@ -0,0 +1,13 @@
+package com.skcc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Test0411EduApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Test0411EduApplication.class, args);
+	}
+
+}
